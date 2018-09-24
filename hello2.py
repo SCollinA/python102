a282ad99620e1dd42176cea49c72016e402dd0e9
Collin Argo <collinargo@gmail.com>
2018-09-24 12:06:20 -0400

    initial commit

---
+++ hello2.py
@@ -0,0 +1,3 @@
+name = input('What is your name? ')
+print('Hello, %s!' % (name.upper()))
+print('Your name has %d letters in it! Awesome!' % (len(name)))
---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input('Give me a noun. ')
+verb = input('Give me a verb. ')
+adverb = input('Give me an adverb. ')
+
+print("%s %s %s" % (noun, verb, adverb))
